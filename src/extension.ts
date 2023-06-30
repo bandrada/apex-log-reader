6e1359c8ad5e7938414c2262beec9a53905eb878
blaine_andrada <blaine.andrada@ctsfw.edu>
2023-06-30 06:45:41 -0400

    Display Output to Window

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "apex-log-reader" is now active!');
+	
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -16,7 +16,12 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('apex-log-reader.read-log', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Apex Log Reader!');
+
+		// vscode.window.showInformationMessage('Hello World from Apex Log Reader!');
+		
+		let log = vscode.window.createOutputChannel('Apex Log Reader');
+		log.show();
+		log.appendLine('This is a test');
 	});
 
 	context.subscriptions.push(disposable);
